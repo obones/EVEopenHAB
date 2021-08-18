c11fbb134ea4aeb3332d65e1dafef967c6bab1fd
OBones <obones@free.fr>
2021-08-18 16:30:40 +0200

    Base files for a platformIO setup in VSCode

---
+++ src/EVEopenHAB.cpp
@@ -0,0 +1,70 @@
+/*
+@file    EVEopenHAB.cpp
+@brief   Contains main namespace definitions
+@date    2021-08-18
+@author  Olivier Sannier
+
+@section LICENSE
+
+MIT License
+
+Copyright (c) 2021 Olivier Sannier
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+#include <Arduino.h>
+#include <sys/time.h>
+#include "EVEopenHAB.h"
+
+namespace EVEopenHAB
+{
+    struct timeval timeAtBoot;
+    const unsigned long BAUD = 115200;            // 57600;      // Baudrate for serial communication.
+
+    void Setup()
+    {
+        delay(250);         // Time needed to switch back from Upload to Console
+        Serial.begin(BAUD); // Initialise the serial port
+
+        Serial.setRxBufferSize(512);
+        Serial.setTimeout(1);
+
+        if (gettimeofday(&timeAtBoot, NULL) != 0) 
+            Serial.println(F("Failed to obtain time"));
+
+        Serial.println(); // ESP "Garbage" message
+        Serial.print(F("Arduino IDE Version :\t"));
+        Serial.println(ARDUINO);
+
+        Serial.print(F("ESP SDK version:\t"));
+        Serial.println(ESP.getSdkVersion());
+
+        Serial.print(F("Sketch File :\t\t"));
+        Serial.println(F(__FILE__)); // "RFLink.ino" version is in 20;00 Message
+        Serial.println(F("Compiled on :\t\t" __DATE__ " at " __TIME__));
+
+        EVEopenHAB::Config::Setup();
+
+        //EVEopenHAB::Portal::Init();
+        EVEopenHAB::Wifi::Setup();
+
+        //EVEopenHAB::Portal::Start();
+    }
+
+    void MainLoop()
+    {
+        EVEopenHAB::Wifi::mainLoop();
+        //OTA::mainLoop();
+    }
+}
